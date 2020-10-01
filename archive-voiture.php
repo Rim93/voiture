3d0d2128e8734a1e0fe76d8cdab625b0de43d43c
Rim <rim.ayadi@edu.univ-fcomte.fr>
2020-10-01 10:03:46 +0200

    création fichier

---
+++ archive-voiture.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+<div class="projets">
+<?php if (have_posts()) : ?>
+<?php while (have_posts()) : the_post(); ?>
+<article class="projet">
+<?php the_post_thumbnail( 'thumbnail' ); ?>
+<h1 class="title">
+<a href="<?php the_permalink(); ?>">
+<?php the_title(); ?>
+</a>
+</h1>
+<?php the_terms( $post->ID, 'marque', 'Marque : ' ); ?><br>
+<?php the_field('dimension'); ?>
+<?php the_field('couleur'); ?>
+<?php the_field('cylindre'); ?>
+<?php the_field('annee'); ?>
+
+
+
+<div class="content">
+<?php the_content(); ?>
+</div>
+</article>
+<?php endwhile; ?>
+<?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
